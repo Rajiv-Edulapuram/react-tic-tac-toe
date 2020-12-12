ae57754b447cfc0dcd9ef3097f2e2a0b04d765f1
Rajiv <rajivedulapram610@gmail.com>
2020-12-12 06:45:20 -0800

    Added Match Drawn functionality

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ class Board extends React.Component {
     const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = (winner=="Draw")?'Draw Match':'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -108,11 +108,21 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  var count=0;
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
+  for (let i = 0; i < squares.length; i++) {
+    if(squares[i]){
+      count=count+1;
+    }
+    if(count == 9){
+      return "Draw";
+    }
+  }
+  
   return null;
 }
